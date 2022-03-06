21e06179b5e129405ab8b43bd874a9c67a449448
superlgtele <superlgtele@naver.com>
2022-03-06 18:32:01 +0900

    Authenticate
    2022.03.06

--- routes/home.js
+++ routes/home.js
@@ -1,119 +1,112 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const passport = require('../config/passport');
+const passport = require("../config/passport");
 const util = {};
 
 // Home
-router.get('/', function(req, res){
-  res.render('home/welcome');
+router.get("/", function (req, res) {
+  res.render("home/welcome");
 });
 
-router.get('/onlinept', function(req, res){
-  res.render('home/making');
+router.get("/onlinept", function (req, res) {
+  res.render("home/making");
 });
 
-router.get('/onlinept/makingone', function(req, res){
-  res.render('home/firstone');
+router.get("/onlinept/makingone", function (req, res) {
+  res.render("home/firstone");
 });
 
-router.get('/onlinept/makingtwo', function(req, res){
-  res.render('home/firsttwo');
+router.get("/onlinept/makingtwo", function (req, res) {
+  res.render("home/firsttwo");
 });
 
-router.get('/onlinept/makingthree', function(req, res){
-  res.render('home/firstthree');
+router.get("/onlinept/makingthree", function (req, res) {
+  res.render("home/firstthree");
 });
 
-router.get('/onlinept/purchase_squat', function(req, res){
-  res.render('home/purchase_squat');
+router.get("/onlinept/purchase_squat", function (req, res) {
+  res.render("home/purchase_squat");
 });
 
-util.isLoggedin = function(req, res, next){
-  if(req.isAuthenticated()){
+util.isLoggedin = function (req, res, next) {
+  if (req.isAuthenticated()) {
     next();
-  }
-  else {
-    req.flash('errors', {login:'로그인을 먼저 해주세요!'});
-    res.redirect('/login');
-  }
-}
-
-router.get('/onlinept/purchase_one', function(req, res){
-
-  // Need update ejs
-
-  // if(req.isAuthenticated()){
-  //   res.render('home/purchase_one')
-  // } else {
-  //   req.flash('errors', {login:'로그인을 먼저 해주세요!'});
-  //   res.redirect('/login');
-  // }
-
-  res.render('home/purchase_one');
-
-});
-
-router.get('/onlinept/purchase_two', function(req, res){
-  if(req.isAuthenticated()){
-    res.render('home/purchase_two')
   } else {
-    req.flash('errors', {login:'로그인을 먼저 해주세요!'});
-    res.redirect('/login');
+    req.flash("errors", { login: "로그인을 먼저 해주세요!" });
+    res.redirect("/login");
+  }
+};
+
+router.get("/onlinept/purchase_one", function (req, res) {
+  if (req.isAuthenticated()) {
+    res.render("home/purchase_one");
+  } else {
+    req.flash("errors", { login: "로그인을 먼저 해주세요!" });
+    res.redirect("/login");
   }
 });
 
-router.get('/onlinept/purchase_three', function(req, res){
-  if(req.isAuthenticated()){
-    res.render('home/purchase_three')
+router.get("/onlinept/purchase_two", function (req, res) {
+  if (req.isAuthenticated()) {
+    res.render("home/purchase_two");
   } else {
-    req.flash('errors', {login:'로그인을 먼저 해주세요!'});
-    res.redirect('/login');
+    req.flash("errors", { login: "로그인을 먼저 해주세요!" });
+    res.redirect("/login");
+  }
+});
+
+router.get("/onlinept/purchase_three", function (req, res) {
+  if (req.isAuthenticated()) {
+    res.render("home/purchase_three");
+  } else {
+    req.flash("errors", { login: "로그인을 먼저 해주세요!" });
+    res.redirect("/login");
   }
 });
 
 // Login
-router.get('/login', function (req,res) {
-  var username = req.flash('username')[0];
-  var errors = req.flash('errors')[0] || {};
-  res.render('home/login', {
-    username:username,
-    errors:errors
+router.get("/login", function (req, res) {
+  var username = req.flash("username")[0];
+  var errors = req.flash("errors")[0] || {};
+  res.render("home/login", {
+    username: username,
+    errors: errors,
   });
 });
 
 // Post Login
-router.post('/login',
-  function(req,res,next){
+router.post(
+  "/login",
+  function (req, res, next) {
     var errors = {};
     var isValid = true;
 
-    if(!req.body.username){
+    if (!req.body.username) {
       isValid = false;
-      errors.username = '아이디를 입력해주세요!';
+      errors.username = "아이디를 입력해주세요!";
     }
-    if(!req.body.password){
+    if (!req.body.password) {
       isValid = false;
-      errors.password = '비밀번호를 입력해주세요!';
+      errors.password = "비밀번호를 입력해주세요!";
     }
 
-    if(isValid){
+    if (isValid) {
       next();
-    }
-    else {
-      req.flash('errors',errors);
-      res.redirect('/login');
+    } else {
+      req.flash("errors", errors);
+      res.redirect("/login");
     }
   },
-  passport.authenticate('local-login', {
-    successRedirect : '/posts',
-    failureRedirect : '/login'
-  }
-));
+  passport.authenticate("local-login", {
+    successRedirect: "/posts",
+    failureRedirect: "/login",
+  })
+);
 
 // Logout
-router.get('/logout', function(req, res) {
+router.get("/logout", function (req, res) {
   req.logout();
-  res.redirect('/');
+  res.redirect("/");
 });
 
 module.exports = router;
